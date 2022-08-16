2543229b7ca2d6496b28a2f0f8e8d3f95df289c6
Roger Keithi Nojiri da Silva <110481359+rogerkeithi@users.noreply.github.com>
2022-08-15 22:19:45 -0300

    Upando atividade

---
+++ mod_arquivos/ordenaArquivos.java
@@ -0,0 +1,92 @@
+package org.example;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Random;
+/*      Abre cada um dos arquivos existentes no conjunto de pastas/arquivos
+        Lê o arquivo e armazena seus dados em um vetor
+        Ordena o vetor
+        Grava os dados desse vetor em um novo arquivo (na mesma pasta) acrescentando "_ord" em seu nome. Exemplo: arq1.txt -> arq1_ord.txt*/
+
+public class Main {
+    public static int N[] = new int[]{100, 1000, 10000, 50000, 100000};
+
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 5; i++) {
+            dirPasta(N[i]);
+        }
+
+
+    }
+    public static void criar_arquivos() throws IOException {
+        for (int i = 0; i < 5; i++) {
+            File pasta = new File(String.valueOf(N[i]));
+
+            if (!pasta.exists()) {
+                pasta.mkdir();
+            }
+            int max = N[i];
+            int min = 0;
+            //pasta.getAbsolutePath() retorna o caminho absoluto da pasta,
+            //adicione o string com o nome do arquivo a ser criado
+            System.out.println(pasta.getAbsolutePath());
+            for(int j = 1; j <= 10; j++){
+                String arqCriacao = pasta.getAbsolutePath()+ "/arq" + j + ".txt";
+                BufferedWriter buffWrite = new BufferedWriter(new FileWriter(arqCriacao, false));
+
+                for(int k = 0; k < N[i]; k++){
+                    Random aleatorio = new Random();
+                    int valor = aleatorio.nextInt(max - min) + min;
+                    buffWrite.append(valor + "\n");
+                }
+                buffWrite.close();
+
+                max = max + max;
+                min = min + N[i];
+            }
+        }
+    }
+    private static void dirPasta(int x) throws IOException {
+        String dirName = String.valueOf(x);
+
+        Files.list(new File(dirName).toPath())
+                .limit(10)
+                .forEach(path -> {
+                    System.out.println(path);
+                    try {
+                        ordenarVetor(lerArquivo("./"+path));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+    public static String[] lerArquivo(String arqLendo) throws IOException {
+        File arq = new File(arqLendo);
+        String arqDir = arq.getAbsolutePath();
+        BufferedReader buffRead = new BufferedReader(new FileReader(arqDir));
+
+        int N = 0;
+
+        while(buffRead.readLine() != null){
+            N += 1;
+        }
+        buffRead.close();
+
+        BufferedReader br = new BufferedReader(new FileReader(arqDir));
+
+        String[] res = new String[N];
+
+        for(int i = 0; i < N; i++){
+            res[i] = br.readLine();
+        }
+
+        br.close();
+
+        return res;
+    }
+
+    public static String[] ordenarVetor(String[] vet){
+        
+    }
+}
